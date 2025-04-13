3de5897b21adc5233743470778c8c48cba32a379
George S. Mulbah <gsmulbah2500@gmail.com>
2025-04-13 12:28:45 +0200

    completed the lifetime challenge in rust

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -1,9 +1,10 @@
 // Lifetimes are also needed when structs hold references.
 
+// In this example, we have a struct `Book` that holds references to a string slice for the author and title.
 // TODO: Fix the compiler errors about the struct.
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
